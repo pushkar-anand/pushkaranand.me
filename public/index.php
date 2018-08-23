3c43118aa3dcf0d6117abbca9d417053ca8acfc3
Pushkar Anand <anandpushkar088@gmail.com>
2018-08-24 02:51:11 +0530

    added route->excute()

--- public/index.php
+++ public/index.php
@@ -8,6 +8,8 @@ $route = new \EasyRoute\Route();
 try{
     $route->addMatch('GET', '/', $home_view);
 
+    $route->execute();
+
 }catch (Exception $exception)
 {
     error_log("Exception: $exception");
