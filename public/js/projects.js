4e078091c3ed6b12b8fd0655003b5308d84ccd81
Pushkar Anand <anandpushkar088@gmail.com>
2018-08-24 21:12:57 +0530

    Added project data and images.

---
+++ public/js/projects.js
@@ -0,0 +1,43 @@
+const projects = [
+    {
+        "name": "Simple Budget",
+        "info": "Easily manage all your money. Track your spends and earnings. Tag you transactions. Add transaction specific notes.",
+        "img": "/images/projects/simple-budget.png",
+        "link": "http://bit.ly/simple-budget-app",
+        "repo": "https://github.com/pushkar-anand/simple-budget"
+    },
+    {
+        "name": "CopyMean",
+        "info": "Reading an article found any word you do not understand? Just copy the word and get the meaning instantly.",
+        "img": "/images/projects/copymean.webp",
+        "link": "http://bit.ly/copymean"
+    },
+    {
+        "name": "birthday-card",
+        "info": "A birthday Card created with pure CSS3 and HTML. No JS To run this card u need to have a browser with full CSS3 support.",
+        "img": "/images/projects/card.png",
+        "link": "http://pushkar-anand.github.io/birthday-card/demo.html",
+        "repo": "https://github.com/pushkar-anand/birthday-card"
+    },
+    {
+        "name": "easy-mail",
+        "info": "A wrapper for php's mail function that allows you to easily construct and send mails.",
+        "img": "/images/projects/easy-mail.png",
+        "link": "https://packagist.org/packages/pushkar/easy-mail",
+        "repo": "https://github.com/pushkar-anand/easy-mail"
+    },
+    {
+        "name": "easy-router",
+        "info": "Easiest way to add route to your php application.",
+        "img": "/images/projects/easy-route.png",
+        "link": "https://packagist.org/packages/pushkar/easy-router",
+        "repo": "https://github.com/pushkar-anand/easy-router"
+    },
+    {
+        "name": "php-useful",
+        "info": "Composer library for various classes and functions to many common tasks in PHP.",
+        "img": "/images/projects/php-useful.png",
+        "link": "https://packagist.org/packages/pushkar/php-useful",
+        "repo": "https://github.com/pushkar-anand/php-useful"
+    }
+];
