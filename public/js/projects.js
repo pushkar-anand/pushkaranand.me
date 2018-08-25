941e0a859d27054c0668136ea46d6efb944aafbc
Pushkar Anand <anandpushkar088@gmail.com>
2018-08-25 19:50:46 +0530

    Design changes.

--- public/js/projects.js
+++ public/js/projects.js
@@ -1,27 +1,20 @@
 const projects = [
     {
         "name": "Simple Budget",
-        "info": "Easily manage all your money. Track your spends and earnings. Tag you transactions. Add transaction specific notes.",
+        "info": "Easily manage all your money. Track your spends and earnings. Tag your transactions. Add transaction specific notes.",
         "img": "/images/projects/simple-budget.png",
         "link": "http://bit.ly/simple-budget-app",
         "repo": "https://github.com/pushkar-anand/simple-budget"
     },
     {
         "name": "CopyMean",
-        "info": "Reading an article found any word you do not understand? Just copy the word and get the meaning instantly.",
+        "info": "Reading something on your phone? Found any word you do not understand? Just copy the word and get the meaning instantly.",
         "img": "/images/projects/copymean.webp",
         "link": "http://bit.ly/copymean"
     },
     {
-        "name": "birthday-card",
-        "info": "A birthday Card created with pure CSS3 and HTML. No JS To run this card u need to have a browser with full CSS3 support.",
-        "img": "/images/projects/card.png",
-        "link": "http://pushkar-anand.github.io/birthday-card/demo.html",
-        "repo": "https://github.com/pushkar-anand/birthday-card"
-    },
-    {
         "name": "easy-mail",
-        "info": "A wrapper for php's mail function that allows you to easily construct and send mails.",
+        "info": "A composer plugin that acts as wrapper for php's mail function that allows you to easily construct and send mails.",
         "img": "/images/projects/easy-mail.png",
         "link": "https://packagist.org/packages/pushkar/easy-mail",
         "repo": "https://github.com/pushkar-anand/easy-mail"
@@ -39,5 +32,12 @@ const projects = [
         "img": "/images/projects/php-useful.png",
         "link": "https://packagist.org/packages/pushkar/php-useful",
         "repo": "https://github.com/pushkar-anand/php-useful"
-    }
+    },
+    {
+        "name": "Birthday Card",
+        "info": "A birthday Card created with pure CSS3 and HTML. No JS To run this card you need to have a browser with full CSS3 support.",
+        "img": "/images/projects/card.png",
+        "link": "http://pushkar-anand.github.io/birthday-card/demo.html",
+        "repo": "https://github.com/pushkar-anand/birthday-card"
+    },
 ];
