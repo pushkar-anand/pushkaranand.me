9d7602f9c3034b027e829cbaa1f76d911b2de652
Pushkar Anand <anandpushkar088@gmail.com>
2018-09-10 17:25:42 +0530

    added mail.

--- php_scripts/contact-mail.php
+++ php_scripts/contact-mail.php
@@ -11,8 +11,7 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) 
     $subject = Functions::escape_input($_POST['subject']);
     $msg = Functions::escape_input($_POST['message']);
 
-    try
-    {
+    try {
 
         $mail = new Mail('anandpushkar088@gmail.com');
         $mail->addReplyTo($email, $name);
@@ -22,18 +21,19 @@ if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['subject']) 
         $mail->setSubject($subject);
         $mail->setMsg($msg);
 
-        if($mail->sendMail())
-        {
-            $resp = array("code" => 200, "msg" => "Mail sent successfully.");
-        }else{
-            $resp = array("code" => 500, "msg" => "Their was a error sending the mail.");
+        if ($mail->sendMail()) {
+            $resp = array("code" => 200, "title" => "Mail sent", "msg" => "Mail was sent. Expect a response in 3hrs.");
+        } else {
+            $resp = array("code" => 500, "title" => "Mail not sent", "msg" => "Their was a error sending the mail.");
         }
 
     } catch (Exception $exception) {
         error_log($exception);
-        $resp = array("code" => 200, "msg" => $exception);
+        $resp = array("code" => 200, "title" => "Error", "msg" => $exception);
     }
 
     EasyHeaders::json_header();
     echo json_encode($resp);
+} else {
+    $resp = array("code" => 500, "title" => "Mail not sent", "msg" => "Fill in all fields.");
 }
---
+++ public/js/forms.js
@@ -0,0 +1,50 @@
+let jqueryErr = function(jqXHR, exception) {
+        let msg = '';
+        if (jqXHR.status === 0) {
+            msg = 'Not connect.\n Verify Network.';
+        } else if (jqXHR.status === 404) {
+            msg = 'Requested page not found. [404]';
+        } else if (jqXHR.status === 500) {
+            msg = 'Internal Server Error [500].';
+        } else if (exception === 'parsererror') {
+            msg = 'Requested JSON parse failed.';
+        } else if (exception === 'timeout') {
+            msg = 'Time out error.';
+        } else if (exception === 'abort') {
+            msg = 'Ajax request aborted.';
+        } else {
+            msg = 'Uncaught Error.\n' + jqXHR.responseText;
+        }
+        swal("Error", msg, "error");
+    };
+
+    let submit = function(formID)
+    {
+      let formSubmit = $("form#"+formID);
+      formSubmit.submit(function(e) {
+      e.preventDefault();
+      let formData = new FormData(this);
+      console.log("Form Submitted");
+      $.ajax(
+          {
+              url: '/contact',
+              dataType: 'json',
+              cache: false,
+              processData: false,
+              data: formData,
+              type: 'post',
+              success: function (response) {
+                  if (response.code === 200) {
+                      swal(response.title, response.msg, "success");
+                      formSubmit[0].reset();
+                  }
+                  else {
+                      swal(response.title, response.msg, "error");
+                  }
+              },
+              error: function (jqXHR, exception) {
+                  jqueryErr(jqXHR, exception);
+              }
+          });
+    });
+  };
